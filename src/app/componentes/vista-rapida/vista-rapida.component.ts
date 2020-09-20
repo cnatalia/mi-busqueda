b0b1817f62acde356c019fbe39fe60f216154a88
Natalia Herrera <natalia.herrera.rico@gmail.com>
2020-09-20 13:52:19 -0500

    tipos adecuados

--- src/app/componentes/vista-rapida/vista-rapida.component.ts
+++ src/app/componentes/vista-rapida/vista-rapida.component.ts
@@ -6,6 +6,8 @@ import { Meta } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
+import { ProductosResponse } from '../../modelos/consulta-productos-response';
+import { DetalleResponse } from '../../modelos/detalle-response';
 import { ConsultarProductosService } from '../../servicios/consultar-productos/consultar-productos.service';
 import { DatosMemoriaService } from '../../servicios/datos-memoria/datos-memoria.service';
 
@@ -25,7 +27,7 @@ export class VistaRapidaComponent implements OnInit {
 
   public items;
   public categorias;
-  public resultado;
+  public resultado: ProductosResponse;
   public mostar: boolean;
   public buscarParametro;
   public prediccion;
@@ -44,7 +46,7 @@ export class VistaRapidaComponent implements OnInit {
     this.activatedRoute.queryParams.subscribe(params => {
       this.buscarParametro = params.search;
       this.metaTagService.updateTag({ name: 'keywords', content: `buscar, ${this.buscarParametro}, ${this.buscarParametro} rojo` });
-      this.metaTagService.updateTag({ name: 'description', content: `Encuentra los mejores ${this.buscarParametro}`});
+      this.metaTagService.updateTag({ name: 'description', content: `Encuentra los mejores ${this.buscarParametro}` });
 
 
       if (this.buscarParametro) {
@@ -57,6 +59,7 @@ export class VistaRapidaComponent implements OnInit {
     return this.consultaService.consultarProductos(buscar)
       .pipe(
         tap(response => {
+          console.log(response);
           this.items = response.results.map((product, i) => ({
             id: product.id,
             title: product.title,
@@ -67,7 +70,8 @@ export class VistaRapidaComponent implements OnInit {
 
             picture: product.thumbnail,
             conditions: product.condition,
-            freeShipping: product.shipping.free_shipping
+            freeShipping: product.shipping.free_shipping,
+            city: product.seller_address.state.name
 
           }));
           this.categorias = Array(response.available_filters[0]).find(res => res.id === 'category') ?
@@ -78,14 +82,18 @@ export class VistaRapidaComponent implements OnInit {
 
               })) : 'No disponible';
 
-          this.resultado = Array(response).map((product, i) => ({
+
+          const arr = Array(response).map((product, i) => ({
             author: {
               name: product.site_id,
-              lastame: product.site_id
+              lastname: product.site_id
             },
             categories: this.categorias,
             items: this.items
           }));
+          this.resultado = Object.assign({}, arr[0]);
+          // this.resultado = Object.assign({}, array[0]);
+
         })
       );
   }
